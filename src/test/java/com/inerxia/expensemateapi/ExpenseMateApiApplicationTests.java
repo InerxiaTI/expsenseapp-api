bd78dd2b5e0461a9a6bef9a3003af1fafb22c596
kellytbn <kellytbn14@gmail.com>
2023-11-02 20:47:50 -0500

    Start project

---
+++ src/test/java/com/inerxia/expensemateapi/ExpenseMateApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inerxia.expensemateapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseMateApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
